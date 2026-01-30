925dd1ed1efe3ea5b8ab9419fc07648afb959580
Ttttt-Tan <fenhui0910@gmail.com>
2026-01-30 13:45:55 +0800

    init

---
+++ src/main/java/org/self/lab/SelfLogicLabBootApplication.java
@@ -0,0 +1,13 @@
+package org.self.lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfLogicLabBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelfLogicLabBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/self/lab/SelfLogicLabBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.self.lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfLogicLabBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
